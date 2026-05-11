1a16a3dd9b60e2be96afe220853cfdddca257462
Wasp <anacsmaciel4@gmail.com>
2026-05-11 11:51:47 -0300

    Primeiro commit

---
+++ bundle_while.py
@@ -0,0 +1,14 @@
+idade = 20
+salario = 1500.75
+altura = 1.80
+genero = "M"
+nome = "Yu Qi"
+print("Dados do Cliente:")
+print("Idade:",idade)
+print("Salário:",salario, "reais")
+print("Altura:",altura)
+print("Gênero:",genero)
+print("Nome:", nome)
+print("-----"*10)
+
+# tranformar em estruturas while
