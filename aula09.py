0e05d878332a60e4ff83edfeb2e12555bfe2561d
Wasp <anacsmaciel4@gmail.com>
2026-05-14 10:04:55 -0300

    Primeiro commit

--- aula09.py
+++ aula09.py
@@ -7,7 +7,7 @@ while option != "0":
     if option == "1":
         print("\n1 - Cadastrar cliente")
         cad = input("\nDigite seu nome completo: ")
-        phone_number = int(input(f"\nBem vindo, {cad}.\nContato: "))
+        phone_number = (input(f"\nBem vindo, {cad}.\nContato: "))
         email = input ("\nAgora insira seu email: ")
         address = input("\nDigite seu endereço: ")
         print("\n=== Cliente cadastrado com sucesso! ☆*: .｡. o(≧▽≦)o .｡.:*☆ ===")
@@ -26,6 +26,35 @@ while option != "0":
         print()
     elif option == "3":
         print("\n3 - Editar cliente")
+        if cliente_cadastrado == True:
+            record2 = input("\nAlterar:\n1 - Nome\n2 - Contato\n3 - Email\n4 - Endereço\n0 - Sair\n")
+            if record2 == "1":
+                newcad = input("Digite novo nome ou aperte Enter para sair: ")
+                if newcad != "":
+                    cad = newcad
+                else:
+                    cad = cad
+            if record2 == "2":
+                new_phone_number = input("Digite o novo contato ou Enter para sair: ")
+                if new_phone_number != "":
+                    phone_number = new_phone_number
+                else:
+                    phone_number = phone_number
+            if record2 == "3":
+                new_email = input("Digite o novo email ou aperte Enter para sair: ")
+                if new_email != "":
+                    email = new_email
+                else:
+                    email = email
+            if record2 == "4":
+                new_address = input("Digite novo endereço ou aperte enter para sair: ")
+                if new_address != "":
+                    address = new_address
+                else:
+                    address = address
+        else:
+            print("== Não há clientes para editar ==")
+            print()
     elif option == "4":
         print("\n4 - Excluir cliente")
     elif option == "0":
