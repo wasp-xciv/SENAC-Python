5c82efe7f08e7aefcf962b7ff7ea030b1bae112f
Wasp <anacsmaciel4@gmail.com>
2026-05-20 09:14:09 -0300

    Primeiro commit

---
+++ aula10.py
@@ -0,0 +1,28 @@
+#For e Range
+
+# for contador in range (1,21):
+#     print("repetiu",contador,"vezes.")
+# print("=== Fim ===")
+
+# # para for, o contador sempre considera o primeiro número, mas NÃO imprime o último.
+# # Comparação com while:
+
+# i = 0
+# while i < 10:
+#     i += 1
+#     print("repetiu",i,"vezes")
+# print("=== Fim ===")
+
+# for contador in range (10,100,10):
+#     print("repetiu",contador,"vezes")
+# print("=== Fim ===")
+
+# from time import sleep
+# for i in range (10,-1,-1):
+#     print("repetiu",i,"vezes")
+#     sleep(0.70)
+
+# For também inspira economia. No exemplo da inserção das notas:
+
+for c in range (1,5):
+    print(input(f"Informe a {c}º nota: "))
