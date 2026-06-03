68585794cedcf398ff39f7146e78d341c5f78229
Wasp <anacsmaciel4@gmail.com>
2026-06-03 09:32:39 -0300

    Primeiro commit

--- aula15_desafio.py
+++ aula15_desafio.py
@@ -5,26 +5,26 @@
 # # Você tem uma tupla com nomes de produtos e outra com seus preços. Seu 
 # # programa deve analisar esses dados. 
 # # Dados iniciais 
-# produtos = ("Arroz", "Feijão", "Macarrão", "Óleo", "Açúcar") 
-# precos = (20, 8, 5, 7, 4) 
+produtos = ("Arroz", "Feijão", "Macarrão", "Óleo", "Açúcar") 
+precos = (20.00, 8.00, 5.00, 7.00, 4.00) 
 # # Objetivos 
 # # 1. Mostrar todos os produtos com seus preços
-# for produto in range(len(produtos)):
-#     print(f"O produto {produtos[produto]} custa R${precos[produto]:,.2f}")
+for produto in range(len(produtos)):                #maneira de correlacionar os index de ambas as listas
+    print(f"O produto {produtos[produto]} custa R${precos[produto]:,.2f}")
 
 # # 2. Mostrar quais produtos custam mais de R$ 10 
-# print()
-# print("Produtos que custam mais de R$10,00:\n")
-# for pos,prod in enumerate(produtos):
-#     if precos[pos] > 10:
-#         print(f"O produto maior que R$10,00 é {prod} e custa R${precos[pos]:,.2f}")
+print()
+print("Produtos que custam mais de R$10,00:")
+for index,prod in enumerate(produtos):   #enumerate pede dois argumentos(índice, valor)
+    if precos[index] > 10:
+        print(f"O produto maior que R$10,00 é {prod} e custa R${precos[index]:,.2f}")
 
 # # 3. Calcular o total da compra
 
-# soma = 0
-# for pos,prod in enumerate(produtos):
-#     soma += precos[pos]
-# print(f"O valor total dos produtos é R${soma:,.2f}")
+total = 0
+for index,prod in enumerate(produtos):     #outra maneira de calcular a soma dos elementos de uma lista
+    total += precos[index]
+print(f"\nO valor total dos produtos é R${total:,.2f}")
 
 
 # Passo a passo 
@@ -54,18 +54,18 @@ for nota in notas: #é importante diferenciar valor de índice
          print(f"Nota - {nota}\nStatus - Aprovado!") #variável 'nota' sozinha, ao invés de inserida dentro de 'notas'
      elif nota >5.0 and nota <7.0:
          print(f"Nota - {nota}\nStatus - Em Recuperação!")
-     elif nota <5.0:
+     else:
         print(f"Nota - {nota}\nStatus - Reprovado!")
 
 # 2. Mostrar a média da turma
 print()
 print("=== Média da turma: ===\n")
 
-media = sum(notas)/len(notas)   #MUITO mais fácil
-# soma = 0
-# for nota in notas:
-#     soma += nota
-# media = soma/len(notas)
+# media = sum(notas)/len(notas)   #MUITO mais fácil
+total = 0
+for nota in range(len(notas)):
+    total += notas[nota]
+media = total/len(notas)
 print(f"A média da turma é {media:.1f}")
     
 
