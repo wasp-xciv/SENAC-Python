221ea1973d78cab80c0c52ef95f35cdfa6648783
Wasp <anacsmaciel4@gmail.com>
2026-06-03 11:56:41 -0300

    Primeiro commit

--- aula16_dicionario.py
+++ aula16_dicionario.py
@@ -29,8 +29,7 @@ print()
 print(pessoa)
 
 for k,v in pessoa.items():
-    print(f"\n{v}")
-
+    print(f"\n{k}:{v}")
 
 #Como adicionar novas chaves e valores em um dicionário?
 pessoa["profissão"] = "presidente"
@@ -56,3 +55,47 @@ for chaves in pessoa:
 for valor in pessoa.values():
     print(valor)
 
+# nome = {}
+# name = input("Digite seu nome:\n")
+# age = input("Digite sua idade:\n")
+# hobby = input("Hobby:\n")
+# nome = {"Nome":name,"Idade":age,"Hobby":hobby}
+# print(nome)
+
+###################################################################################################
+print()
+pessoa["notas"] = [7.0,8.5,9.0]
+print(pessoa["notas"])
+media = sum(pessoa["notas"])/len(pessoa["notas"])
+print(f"{media:,.1f}")
+
+
+#O que fazer se surgir a dúvida se uma chave existe ou foi apagada
+# print(pessoa["profissão"])  #foi deletado
+print()
+print(pessoa.get("Profissão","Chave não encontrada (；′⌒`)"))
+
+#Desafio
+
+#Nível Básico:
+# Missão: Criar um dicinário com nome, idade e cidade de uma pessoa. Exibir dados na tela.
+
+profile = {"Nome":"Li Shen","Idade":29,"Cidade":"Linkon"}
+print(f"Nome: {profile['Nome']}\nIdade:{profile["Idade"]} anos\nCidade: {profile["Cidade"]}")
+
+#Nível Intermediário:
+#Missão: Criar um dicionário com nome e uma lista de 3 notas. Calcular e mostrar a média.
+
+profile["Notas"] = [9.5,10.0,10.0]
+media = sum(profile["Notas"])/len(profile["Notas"])
+print(f"Notas:{profile["Notas"]}\nA média é {media:,.1f}.")
+
+#Nível Avançado:
+#Missão: Criar uma lista contendo 3 alunos (dicionários). Percorrer a lista e exibir o nome e nota de cada um.
+student1 = {"Aluno":"Li Shen","Notas":[10.0,10.0,10.0]}
+student2 = {"Aluno":"Qin Che","Notas":[8.5,7.9,9.0]}
+student3 = {"Nome":"Qi Yu","Notas":[7.3,7.0,9.5]}
+class_LIS = [student1,student2,student3]
+
+for student in class_LIS:
+    print(student)
