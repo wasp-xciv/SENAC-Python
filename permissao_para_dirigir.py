c4f5ba59105f7f84596e6f49343d6e5f5b63cdf0
Wasp <anacsmaciel4@gmail.com>
2026-06-22 08:37:02 -0300

    Primeiro commit

--- permissao_para_dirigir.py
+++ permissao_para_dirigir.py
@@ -1,17 +1,88 @@
-from datetime import date
-birth = int(input("Insira seu ano de nascimento: "))
-current_year = date.today().year
-age = (current_year - birth)
+# from datetime import date
+# birth = int(input("Insira seu ano de nascimento: "))
+# current_year = date.today().year
+# age = (current_year - birth)
 
-print("\nVocê tem ",age,"anos.")
+# print("\nVocê tem ",age,"anos.")
 
-print("\n=== Status Habilitação ===")
-print()
-if age == 17:
-    print("\nVocê não tem a idade mínima para dirigir.\nFalta 1 ano.")
-elif age < 18:
-    print("\nVocê não tem a idade mínima para dirigir.\nFaltam",18 - age,"anos.")
-elif age == 18 and age <= 20:
-    print("\nVocê está autorizado a solicitar habilitação para categorias A e B.")
-else:
-    print("\nVocê está autorizado a solicitar qualquer categoria de habilitação (A, B, C, D e E).")
+# print("\n=== Status Habilitação ===")
+# print()
+# if age == 17:
+#     print("\nVocê não tem a idade mínima para dirigir.\nFalta 1 ano.")
+# elif age < 18:
+#     print("\nVocê não tem a idade mínima para dirigir.\nFaltam",18 - age,"anos.")
+# elif age == 18 and age <= 20:
+#     print("\nVocê está autorizado a solicitar habilitação para categorias A e B.")
+# else:
+#     print("\nVocê está autorizado a solicitar qualquer categoria de habilitação (A, B, C, D e E).")
+
+
+numeros = "Python"
+# Usando índices negativos e passo, extraia:
+# [9, 7, 5, 3, 1] (elementos em posições ímpares começando do final)
+resultado = numeros[-1:1:-1]
+print(resultado)
+ 
+
+
+ 
+
+ 
+
+
+
+
+ 
+
+
+
+ 
+
+ 
+
+ 
+
+ 
+
+
+ 
+
+ 
+
+ 
+
+
+
+
+
+
+ 
+
+ 
+
+
+ 
+
+
+
+ 
+
+ 
+
+
+ 
+
+
+
+
+ 
+
+ 
+
+
+ 
+
+
+ 
+
+ 
