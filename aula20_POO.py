8b4941c511800b507fc3a126b00fc43eb39aee47
Wasp <anacsmaciel4@gmail.com>
2026-07-01 10:22:13 -0300

    Primeiro commit

---
+++ aula20_POO.py
@@ -0,0 +1,23 @@
+'''
+Classe = molde
+Objeto = algo criado a partir do molde
+
+Analogia:
+Classe Aluno -> alunos Shen Li, Qi Yu, Xia Yazhou (objetos)
+'''
+
+# Criando um objeto da classe Aluno
+
+class Aluno:
+    pass #segue o código a se fazer
+
+aluno1 = Aluno()
+aluno2 = Aluno()
+
+#Atribb (dados dos objetos) são as características do objeto.
+
+aluno1.nome = "Ana"
+aluno1.idade = 17
+
+print(aluno1.nome)
+print(aluno1.idade)
