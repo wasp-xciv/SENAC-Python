e42d144658ed0178daf9f10d34dd4b23955ad1bb
Wasp <anacsmaciel4@gmail.com>
2026-07-02 10:47:50 -0300

    Primeiro commit

--- aula21_cad_estudantes.py
+++ aula21_cad_estudantes.py
@@ -39,4 +39,47 @@ print(f"\nTotal de estudantes cadastrados: {Estudante.total_estudantes}")
 print(125*"*")
 
 #Agora sem declarar parâmetros:
+class Cliente:
+    def __init__(self):
+        #attribs
+        self.codigo = ""
+        self.nome = ""
+        self.telefone = ""
+        #métodos de instância
+    
+    def cadastrar_cliente(self):
+        #Lógica para cadastrar cliente (não é realmente a melhor...)
+        self.codigo = input("Digite o código do cliente: ")
+        self.nome = input("Digite nome do cliente: ")
+        self.telefone = input("Digite o telefone do cliente: ")
+        print("Cliente cadastrado com sucesso!\n")
 
+    def mostrar_dados(self):
+        print(f"Código: {self.codigo}")
+        print(f"Nome: {self.nome}")
+        print(f"Telefone: {self.telefone}")
+    
+    def atualizar_cliente(self):
+        #Lógica para atualizar os dados do cliente
+        self.codigo = input("\nDigite o novo código do cliente: ")
+        self.nome = input("Digite o novo nome do cliente: ")
+        self.telefone = input("Digite o novo telefone do cliente: ")
+        print("Cliente atualizado com sucesso!")
+
+    def excluir_cliente(self):
+        select_cod = input("\nDigite o código do cliente que deseja excluir: ")
+        if select_cod == self.codigo:
+            self.codigo = ""
+            self.nome = ""
+            self.telefone = ""
+            print("Cliente excluído com sucesso!")
+        else:
+            print("Código do cliente não encontrado.")
+    
+#Criando o maldito clienteeeeeeeeeeeeeeeeeeee
+
+cliente1 = Cliente()
+cliente1.cadastrar_cliente()
+cliente1.mostrar_dados()
+cliente1.atualizar_cliente()
+cliente1.excluir_cliente()
