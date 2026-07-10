b4cf7da2cf5d0c24fddcd8c8771bfce21416f19d
wasp-xciv <anacsmaciel4@gmail.com>
2026-07-09 23:06:53 -0300

    Primeiro commit

--- aula23.py
+++ aula23.py
@@ -11,27 +11,27 @@ rotulo.place(x=0,y=0,width=800,height=100)
 rotulo = tk.Label(janela,text="»»»» Nome:",font=("Courier",12,"bold"))        #acrescenta mais um label abaixo do primeiro
 rotulo.place(x=30,y=120,width=100,height=30)
 caixa_nome = tk.Entry(janela,font=("Courier",10,"bold"),fg="#110811")
-caixa_nome.place(x=170,y=120,width=400,height=30)
+caixa_nome.place(x=175,y=120,width=400,height=30)
 
 rotulo = tk.Label(janela,text="»»»» Endereço:",font=("Courier",12,"bold"))
 rotulo.place(x=30,y=160,width=140,height=30)
 caixa_address = tk.Entry(janela,font=("Courier",10,"bold"),fg="#110811")
-caixa_address.place(x=170,y=160,width=400,height=30)
+caixa_address.place(x=175,y=160,width=400,height=30)
 
 rotulo = tk.Label(janela,text="»»»» Telefone:",font=("Courier",12,"bold"))
 rotulo.place(x=30,y=200,width=140,height=30)
 caixa_phone = tk.Entry(janela,font=("Courier",10,"bold"),fg="#110811")
-caixa_phone.place(x=170,y=200,width=400,height=30)
+caixa_phone.place(x=175,y=200,width=400,height=30)
 
 rotulo = tk.Label(janela,text="»»»» E-mail:",font=("Courier",12,"bold"))
 rotulo.place(x=30,y=240,width=110,height=30)
 caixa_address = tk.Entry(janela,font=("Courier",10,"bold"),fg="#110811")
-caixa_address.place(x=170,y=240,width=400,height=30)
+caixa_address.place(x=175,y=240,width=400,height=30)
 
 rotulo = tk.Label(janela,text="»»»» CPF:",font=("Courier",12,"bold"))
 rotulo.place(x=30,y=280,width=80,height=30)
 caixa_address = tk.Entry(janela,font=("Courier",10,"bold"),fg="#110811")
-caixa_address.place(x=170,y=280,width=400,height=30)
+caixa_address.place(x=175,y=280,width=400,height=30)
 
 botao1 = tk.Button(janela,text="Salvar",font=("Courier",10,"bold"),bg="#F5EFF5")
 botao1.place(x=30, y=550, width = 100, height=30)
